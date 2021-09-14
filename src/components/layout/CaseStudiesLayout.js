90020d92588ea8f24027a22d54773d3b9297a7a6
Joe <joe@startdigital.com.au>
2021-09-14 09:25:01 +0800

    fix typo

--- src/components/layout/CaseStudiesLayout.js
+++ src/components/layout/CaseStudiesLayout.js
@@ -3,7 +3,7 @@ import InternalLayout from "./InternalLayout"
 import { ChevronRightIcon } from '@heroicons/react/outline'
 
 const CaseStudiesLayout = ({ pageContext }) => {
-    const frontMatter = pageContext.frontmattern
+    const frontMatter = pageContext.frontmatter
     return (
         <InternalLayout title={frontMatter.client}>
             <div className="px-4 lg:px-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
