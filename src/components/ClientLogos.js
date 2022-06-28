eef7bb870f68c4e32e2d95518cd5805f283ea52e
Joe Grant <jmgrant1995@gmail.com>
2022-06-28 12:43:10 +0800

    fix case sensitivities which work locally but break on prod

--- src/components/ClientLogos.js
+++ src/components/ClientLogos.js
@@ -6,19 +6,19 @@ export default function ClientLogos({ classes }) {
     <section className={`max-w-7xl mx-auto px-4 sm:px-6 ${classes}`}>
       <div className="flex items-center space-x-5">
           <div data-animate-right className="flex-1">
-            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Singtel-logo.png'} alt="Singtel logo" />
+            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Singtel-Logo.png'} alt="Singtel logo" />
           </div>
           <div data-animate-right className="flex-1">
-            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Gorgi-logo.png'} alt="Gorgi logo" />
+            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Gorgi-Logo.png'} alt="Gorgi logo" />
           </div>
           <div data-animate-right className="flex-1">
-            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Goldfields-logo.png'} alt="Goldfields Money logo" />
+            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Goldfields-Logo.png'} alt="Goldfields Money logo" />
           </div>
           <div data-animate-right className="flex-1">
-            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Fremantle-Dockers-logo.png'} alt="Fremantle Dockers logo" />
+            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Fremantle-Dockers-Logo.png'} alt="Fremantle Dockers logo" />
           </div>
           <div data-animate-right className="flex-1">
-            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Gumala-logo.png'} alt="Gumala logo" />
+            <StaticImage className="client_logo md:px-0 px-2" objectFit="contain" width={150} src={'../images/logos/Gumala-Logo.png'} alt="Gumala logo" />
           </div>
       </div>
     </section>
