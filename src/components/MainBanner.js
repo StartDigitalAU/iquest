a413303814f32a0156a90aaf9c4b8936b8c6a6e0
Tom Brennan <67259512+tomcbrennan@users.noreply.github.com>
2023-02-27 12:29:18 +0800

    Gatsby video added

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -1,10 +1,28 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import ClientLogos from "./ClientLogos"
-import PerlinNoise from "./PerlinNoise"
-import BannerVideo from "../videos/iquest_video_loop.mp4"
+// import PerlinNoise from "./PerlinNoise"
+import ReactPlayer from 'react-player';
 
 export default function MainBanner() {
+
+  const BannerVideo = () => {
+    return (
+      <div>
+        <ReactPlayer
+          url='https://www.dropbox.com/s/9k9xx61o8fl4tgr/iquest_video_loop.mp4?dl=0'
+          playing={true}
+          loop={true}
+          muted={true}
+          width='100%'
+          height='100%'
+          className="absolute top-[-200px] left-0 w-screen"
+        />
+      </div>
+    );
+  };
+
+
   return (
     <div className="pb-16 lg:pb-32 xl:pb-24 pt-36 md:pt-48 relative">
       {/* Video */}
@@ -13,7 +31,7 @@ export default function MainBanner() {
         <div className="container max-xl:max-w-5xl mx-auto flex flex-col gap-6">
           <div className="max-w-3xl flex flex-col gap-6 lg:gap-6 mb-10 lg:mb-0">
             <h2 className="text-sm font-sans inline-block md:text-lg tracking-wide text-purple">
-              18 Years of Exception Perth IT Support
+              18 Years of Exceptional Perth IT Support
             </h2>
             <h1 className="text-4xl leading-normal lg:text-6xl">
               Future Focused
@@ -30,10 +48,7 @@ export default function MainBanner() {
           </div>
         </div>
       </div>
-      <video autoplay muted loop className="hidden xl:block absolute top-0 left-0 w-screen">
-        <source src={BannerVideo} type="video/mp4"/>
-        Your browser does not support the video tag.
-      </video>
+      <BannerVideo />
 
       {/* <PerlinNoise /> */}
       {/* <div className="absolute top-0 right-0 transform translate-x-1/4 lg:w-3/4 xl:w-3/5 ">
